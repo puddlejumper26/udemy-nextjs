40d78b66b886722f27f0694b834d62847af55690
Xiang Wu <xiang.wu@hotmail.de>
2023-05-31 16:56:54 +0200

    [max-app] apply MD transfer and read through local MD and next component

---
+++ max-app/lib/posts-util.js
@@ -0,0 +1,41 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+const postsDirectory = path.join(process.cwd(), "posts");
+
+function getPostData(fileName) {
+  const filePath = path.join(postsDirectory, fileName);
+  const fileContent = fs.readFileSync(filePath, "utf-8");
+  const { data, content } = matter(fileContent);
+
+  const postSlug = fileName.replace(/\.md$/, ""); // remove the file extension
+  const postData = {
+    slug: postSlug,
+    ...data,
+    content: content,
+  };
+
+  return postData;
+}
+
+export function getAllPosts() {
+  const postFiles = fs.readdirSync(postsDirectory);
+
+  //   for (const postFile of postFiles) {
+  //     const postData = getPostData(postFile);
+  //   }
+  const allPosts = postFiles.map((postFile) => {
+    return getPostData(postFile);
+  });
+
+  const sortedPosts = allPosts.sort((a, b) => (a.data > b.data ? -1 : 1));
+
+  return sortedPosts;
+}
+
+export function getFeaturedPosts() {
+  const allPosts = getAllPosts();
+  const featuredPosts = allPosts.filter((post) => post.isFeatured);
+  return featuredPosts;
+}
---
+++ max-app/posts/get-featured-posts1.md
@@ -0,0 +1,11 @@
+---
+title: "This is the title"
+date: "2023-02-10"
+image: get-featured-post.jpeg
+excerpt: "This is the excerpt"
+isFeatured: true
+---
+
+# This is a title
+
+This is some regular text with ....
