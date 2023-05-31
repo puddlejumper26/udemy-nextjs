fb1df9267de40bf770f3b7d6faff05c04aef9203
Xiang Wu <xiang.wu@hotmail.de>
2023-05-31 11:29:16 +0200

    [max-app] adding dummy data, postItem, postGrid, mainNavigation, featuredPost components

--- max-app/pages/index.js
+++ max-app/pages/index.js
@@ -1,10 +1,43 @@
 import { Fragment } from "react";
 import Hero from "../components/home-page/hero";
+import FeaturedPosts from "../components/home-page/featured-posts";
+
+const DUMMY_POSTS = [
+  {
+    slug: "get-featured-posts1",
+    title: "Get featured posts1",
+    image: "get-featured-post.jpeg",
+    excerpt: "This is the excerpt of the featured 1",
+    date: "2023-05-10",
+  },
+  {
+    slug: "get-featured-posts2",
+    title: "Get featured posts2",
+    image: "get-featured-post.jpeg",
+    excerpt: "This is the excerpt of the featured 2",
+    date: "2023-05-20",
+  },
+  {
+    slug: "get-featured-posts3",
+    title: "Get featured posts3",
+    image: "get-featured-post.jpeg",
+    excerpt: "This is the excerpt of the featured 3",
+    date: "2023-05-30",
+  },
+  {
+    slug: "get-featured-posts4",
+    title: "Get featured posts4",
+    image: "get-featured-post.jpeg",
+    excerpt: "This is the excerpt of the featured 4",
+    date: "2023-05-40",
+  },
+];
 
 function HomePage() {
   return (
     <Fragment>
       <Hero />
+      <FeaturedPosts posts={DUMMY_POSTS} />
     </Fragment>
   );
 }
