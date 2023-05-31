ca89a2c245aa6f85a6c6ccc05d0359dcfa6258da
Xiang Wu <xiang.wu@hotmail.de>
2023-05-31 15:15:29 +0200

    [max-app] adding allPost, postContent, postHeader components

---
+++ max-app/components/posts/post-detail/post-content.js
@@ -0,0 +1,22 @@
+import PostHeader from "./post-header";
+import classes from "./post-content.module.css";
+
+const DUMMY_POST = {
+  slug: "get-featured-posts4",
+  title: "Get featured posts4",
+  image: "get-featured-post.jpeg",
+  date: "2023-05-40",
+  content: "# this is the first featured post",
+};
+
+function PostContent() {
+  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+  return (
+    <article className={classes.content}>
+      <PostHeader title={DUMMY_POST.title} image={imagePath} />
+      {DUMMY_POST.content}
+    </article>
+  );
+}
+
+export default PostContent;
--- max-app/pages/posts/index.js
+++ max-app/pages/posts/index.js
@@ -1,2 +1,37 @@
-function AllPostsPage() {}
+import AllPosts from "../../components/posts/all-posts";
+
+const DUMMY_POSTS = [
+  {
+    slug: "get-featured-posts1",
+    title: "Get featured posts1",
+    image: "get-featured-post.jpeg",
+    excerpt: "This is the excerpt of the featured 1",
+    date: "2023-05-10",
+  },
+  {
+    slug: "get-featured-posts2",
+    title: "Get featured posts2",
+    image: "get-featured-post.jpeg",
+    excerpt: "This is the excerpt of the featured 2",
+    date: "2023-05-20",
+  },
+  {
+    slug: "get-featured-posts3",
+    title: "Get featured posts3",
+    image: "get-featured-post.jpeg",
+    excerpt: "This is the excerpt of the featured 3",
+    date: "2023-05-30",
+  },
+  {
+    slug: "get-featured-posts4",
+    title: "Get featured posts4",
+    image: "get-featured-post.jpeg",
+    excerpt: "This is the excerpt of the featured 4",
+    date: "2023-05-40",
+  },
+];
+
+function AllPostsPage() {
+  return <AllPosts posts={DUMMY_POSTS} />;
+}
 export default AllPostsPage;
