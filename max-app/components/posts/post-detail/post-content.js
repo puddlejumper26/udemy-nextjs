338a869d19622aecfe814dcb3a40e3867d78e993
Xiang Wu <xiang.wu@hotmail.de>
2023-06-04 22:54:13 +0200

    [max-app] add script snipt for MD files

--- max-app/components/posts/post-detail/post-content.js
+++ max-app/components/posts/post-detail/post-content.js
@@ -1,8 +1,8 @@
 import ReactMarkdown from "react-markdown";
 import Image from "next/image";
 
-// import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-// import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
