aa517196ea1f039999792f68c1c8e93399cf0591
Xiang Wu <xiang.wu@hotmail.de>
2023-06-23 06:06:27 +0200

    [max-authentication] add client-side page guard(route protection)

--- max-authetication/components/profile/user-profile.js
+++ max-authetication/components/profile/user-profile.js
@@ -1,8 +1,26 @@
-import ProfileForm from './profile-form';
-import classes from './user-profile.module.css';
+import { useSession, getSession } from "next-auth/client";
+import ProfileForm from "./profile-form";
+import classes from "./user-profile.module.css";
+import { useEffect, useState } from "react";
 
 function UserProfile() {
-  // Redirect away if NOT auth
+  const [session, loading] = useSession();
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedSession, setLoadedSession] = useState();
+
+  useEffect(() => {
+    getSession().then((session) => {
+      if (!session) {
+        window.location.href = "/auth";
+      } else {
+        setIsLoading(false);
+      }
+    });
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading....</p>;
+  }
 
   return (
     <section className={classes.profile}>
