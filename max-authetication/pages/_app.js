30f0e7c267bb3f5d54b508dd6711d6fc8d6d8488
Xiang Wu <xiang.wu@hotmail.de>
2023-06-26 06:34:32 +0200

    [max-authentication] next-auth Session Provider component

--- max-authetication/pages/_app.js
+++ max-authetication/pages/_app.js
@@ -1,11 +1,21 @@
-import Layout from '../components/layout/layout';
-import '../styles/globals.css';
+import { Provider } from "next-auth/client";
+
+import Layout from "../components/layout/layout";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    /**
+     *  @  https://www.bilibili.com/video/BV1Nv41187TA?p=209&vd_source=71f123c00025c38afb1d6d26e9d8742e
+     *  @Provider
+     *   e.g. profilePage has this session, allows next to skip extra session check performed by use session
+     *       if we already have the session from getServerSideProp function
+     */
+    <Provider session={pageProps.session}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   );
 }
 
